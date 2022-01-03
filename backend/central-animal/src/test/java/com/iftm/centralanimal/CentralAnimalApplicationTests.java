937fe40b76090063da5b088d87a09f0b191027d8
Henrique Moreira <henriquemoreiraa@gmail.com>
2022-01-03 12:04:52 -0300

    inicia backend e cria gitignore

---
+++ backend/central-animal/src/test/java/com/iftm/centralanimal/CentralAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.centralanimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralAnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
