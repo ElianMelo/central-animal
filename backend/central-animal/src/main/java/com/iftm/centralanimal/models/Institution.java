ea258c6998a47cec5ded8bdb13949502d8231842
Henrique Moreira <henriquemoreiraa@gmail.com>
2022-01-06 21:55:35 -0300

    cria pastas e classes

---
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/models/Institution.java
@@ -0,0 +1,36 @@
+package com.iftm.centralanimal.models;
+
+import java.io.Serializable;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="institution")
+@Data
+public class Institution implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String description;
+	private String address;
+	private String whatsapp;
+	private String pix;
+	private Boolean portion;
+	private Boolean medicines;
+	private Boolean cleaningMaterial;
+	private Blob institutionImage;
+	
+	@OneToOne
+	private Administrator administrator;
+	
+	@OneToMany
+	private List<Animal> animals = new ArrayList<Animal>();
+}
---
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/models/enums/AnimalType.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.iftm.centralanimal.models.enums;
+
+/**
+ * @author Henrique Moreira
+ *
+ */
+public enum AnimalType {
+	CAT, DOG;
+}
