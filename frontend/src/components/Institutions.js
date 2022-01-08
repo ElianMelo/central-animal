e546277c5da8ab44257e497c1471e4d1f5618932
ElianMelo <elianmelomorais@gmail.com>
2022-01-08 17:13:04 -0300

    Criação da página Instituições

--- frontend/src/components/Institutions.js
+++ frontend/src/components/Institutions.js
@@ -3,41 +3,160 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Image,
+    TouchableOpacity,
+    FlatList
 } from 'react-native';
+
+import Footer from './Footer';
+
+const image = require('../../assets/predio1.jpg');
+
+const DATA = [
+    {
+        id: '1',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '2',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '3',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '4',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '5',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '6',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '7',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '8',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+    {
+        id: '9',
+        txt: 'S.O.S Vida e Resgate',
+        img: image,
+        adress: 'Uberlândia, Santa Mônica'
+    },
+];
+
 export default class Institutions extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             props: props,
-            titleText: "Instituições",
         };
     }
 
+    renderItem = ({ item }) => (
+        <TouchableOpacity style={styles.cardBox}>
+            <View style={styles.cardImageLine}>
+                <Image
+                    style={styles.roundCardImage}
+                    source={item.img}
+                />
+                <Text style={styles.titleTxt}>
+                    {item.txt}
+                </Text>
+            </View>
+            <Text style={styles.adressTxt}>{item.adress}</Text>
+        </TouchableOpacity>
+    );
+
     render() {
         return (
-            <View>
-                <View style={styles.headerBox}>
-                    <Text style={styles.h1Text}>
-                        {this.state.titleText}
-                    </Text>
-                </View>
+            <View style={styles.body}>
+                
+                <FlatList
+                    data={DATA}
+                    style={styles.bottomMargin}
+                    renderItem={this.renderItem}
+                    keyExtractor={item => item.id}
+                />
+
+                <Footer/>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    headerBox: {
-        textAlign: "center",
-        marginBottom: 25,
+    body: {
+        height: "100%"
     },
-    h1Text: {
-        fontSize: 32,
+    bottomMargin: {
+        marginBottom: 70
+    },
+    cardBox: {
+        textAlign: "center",
+        marginHorizontal: 16,
+        marginVertical: 10,
+        padding: 16,
+        borderRadius: 16,
+        backgroundColor: "white",        
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+    },
+    cardImageLine: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleTxt: {
+        marginRight: 'auto',
+        fontSize: 24,
         margin: 16,
         fontWeight: "bold",
         textAlign: "center",
-        color: "#000"
+        color: "black"
+    },
+    adressTxt: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: "black"
+    },
+    roundCardImage: {
+        marginRight: 'auto',
+        width: 60,
+        height: 60,
+        borderRadius: 30
     },
 });
