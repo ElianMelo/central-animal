2775f5f312115aeb7f320b39076a2020c711f22f
Henrique Moreira <henriquemoreiraa@gmail.com>
2022-01-10 01:05:50 -0300

    configura camada de controle e adiciona avisos de requisição, adiciona classe de documentação swagger e configuração de cors

---
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/controllers/AdministratorController.java
@@ -0,0 +1,44 @@
+package com.iftm.centralanimal.controllers;
+
+import com.iftm.centralanimal.models.Administrator;
+import com.iftm.centralanimal.services.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/administrator")
+public class AdministratorController {
+
+    @Autowired
+    private AdministratorService service;
+
+    @GetMapping
+    public List<Administrator> administrators() {
+        return service.allAdministrators();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> newAnimal(@Valid @RequestBody Administrator entity) {
+        service.newAdministrator(entity);
+        return ResponseEntity.ok("Animal cadastrado com sucesso.");
+    }
+
+    @PutMapping("/{id}")
+    public Administrator updateAdministrator(@PathVariable Integer id, @RequestBody Administrator entity) {
+        return service.updateAdministratorById(id, entity);
+    }
+
+    @GetMapping("/{id}")
+    public Administrator findByIdAnimal(@PathVariable Integer id) {
+        return service.findAdministratorById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAdministratorById(@PathVariable Integer id) {
+        service.deleteAdministratorById(id);
+    }
+}
---
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/controllers/InstitutionController.java
@@ -0,0 +1,44 @@
+package com.iftm.centralanimal.controllers;
+
+import com.iftm.centralanimal.models.Institution;
+import com.iftm.centralanimal.services.InstitutionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/institution")
+public class InstitutionController {
+
+    @Autowired
+    private InstitutionService service;
+
+    @GetMapping
+    public List<Institution> institutions() {
+        return service.allInstitutions();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> newAnimal(@Valid @RequestBody Institution entity) {
+        service.newInstitution(entity);
+        return ResponseEntity.ok("Instituição cadastrada com sucesso.");
+    }
+
+    @PutMapping("/{id}")
+    public Institution updateInstitution(@PathVariable Integer id, @RequestBody Institution entity) {
+            return service.updateInstitutionById(id, entity);
+    }
+
+    @GetMapping("/{id}")
+    public Institution findInstitutionById(@PathVariable Integer id) {
+        return service.findInstitutionById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteInstitutionById(@PathVariable Integer id) {
+        service.deleteInstitutionById(id);
+    }
+}
