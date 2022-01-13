e01f060604576c4ae04389b3a7cab36563d24e49
Henrique Moreira <henriquemoreiraa@gmail.com>
2022-01-12 21:06:14 -0300

    adiciona parametro no aplication.properties para utilização do Data.sql

---
+++ backend/central-animal/src/main/resources/Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO ADMINISTRATOR(email, password) VALUES ('maria@yahoo.com.br', 'maria@123');
+INSERT INTO ADMINISTRATOR(email, password) VALUES ('jonas@gmail.com', 'games@321123');
+INSERT INTO ADMINISTRATOR(email, password) VALUES ('pedrinho_2019@hotmail.com', '2019_2020');
+INSERT INTO ADMINISTRATOR(email, password) VALUES ('maria_gatinha@gmail.com', 'cleber@mariazinha');
+INSERT INTO ADMINISTRATOR(email, password) VALUES ('casimiro2011@gmail.com', 'brasilcampeao@2022');
+
+INSERT INTO institution_address(city, district, public_place, public_place_name, number) VALUES ('Uberlândia', 'Santa Mônica', 'Rua', 'Alvira', 225);
+INSERT INTO institution_address(city, district, public_place, public_place_name, number) VALUES ('Uberlândia', 'São Gabriel', 'Rua', 'das Garças', 862);
+INSERT INTO institution_address(city, district, public_place, public_place_name, number) VALUES ('Uberlândia', 'Segismundo Pereira', 'Rua', 'Domingos de Freitas', 1041);
+INSERT INTO institution_address(city, district, public_place, public_place_name, number) VALUES ('Uberlândia', 'Monte Hebrom', 'Rua', 'Antônio Marcos Galvão', 981);
+INSERT INTO institution_address(city, district, public_place, public_place_name, number) VALUES ('Uberlândia', 'Pacaembu', 'Rua', 'Tônico Carrijo', 564);
