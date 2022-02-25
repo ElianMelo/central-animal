614d08990f4adfdaae6df6b46ede7ecfb459dc8c
henriquema <henriquema@algartelecom.com.br>
2022-02-25 00:13:19 -0300

    cria classes e métodos para a geração de token de acesso

---
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/security/JWTFilterAutenticate.java
@@ -0,0 +1,68 @@
+package com.iftm.centralanimal.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iftm.centralanimal.data.DetailAdministratorData;
+import com.iftm.centralanimal.models.Administrator;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JWTFilterAutenticate extends UsernamePasswordAuthenticationFilter {
+
+    public static final int EXPIRATION_TOKEN = 600_000;
+
+//    a senha está aqui para fins de desenvolvimento, em um momento futuro estará em algum arquivo fora do repositório.
+    public static final String PASSWORD_TOKEN = "af305f04-2fb6-4322-a652-bd771436fd35";
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            Administrator administrator = new ObjectMapper()
+                    .readValue(request.getInputStream(), Administrator.class);
+
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    administrator.getEmail(),
+                    administrator.getPassword(),
+                    new ArrayList<>()
+            ));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Fala ao autenticar o administrador", e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException,
+                                                                                ServletException {
+
+
+        DetailAdministratorData administratorData = (DetailAdministratorData) authResult.getPrincipal();
+
+        String token = JWT.create()
+                .withSubject(administratorData.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TOKEN))
+                .sign(Algorithm.HMAC512(PASSWORD_TOKEN));
+
+        response.getWriter().write(token);
+        response.getWriter().flush();
+    }
+}
