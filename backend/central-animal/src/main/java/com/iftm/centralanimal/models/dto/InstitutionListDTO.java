43329216dbf961e5cf09b62cbe9a704582f1170b
luansantos <luan.assis05.santos@gmail.com>
2022-05-15 19:21:27 -0300

    Ajustando nome de propriedade de imagem para DTO de lista de instituições

--- backend/central-animal/src/main/java/com/iftm/centralanimal/models/dto/InstitutionListDTO.java
+++ backend/central-animal/src/main/java/com/iftm/centralanimal/models/dto/InstitutionListDTO.java
@@ -14,14 +14,14 @@ public class InstitutionListDTO {
     private String publicPlace;
     private String publicPlaceName;
     private String institutionNumber;
-    private String institutionImage;
+    private String image;
     
     public InstitutionListDTO(Institution institution) {
         this.id = institution.getId();
         this.name = institution.getName();
         this.city = institution.getAddress().getCity();
         this.district = institution.getAddress().getDistrict();
-        this.institutionImage = institution.getImage();
+        this.image = institution.getImage();
         this.publicPlace = institution.getAddress().getPublicPlace();
         this.institutionNumber = institution.getAddress().getNumber();
         this.publicPlaceName = institution.getAddress().getPublicPlaceName();
